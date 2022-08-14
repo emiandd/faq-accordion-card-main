64d5cc012d9653ce7bad01b2ed18b7983fb54566
emiandd <emilandresaravena@icloud.com>
2022-08-13 23:10:09 -0400

    component: ready for desktop 1440

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+let questions =  document.querySelectorAll('.question');
+let arrows = document.querySelectorAll('.arrow');
+let answers = document.querySelectorAll('.answer');
+
+questions = Object.entries(questions);
+arrows = Object.entries(arrows);
+answers = Object.entries(answers);
+
+for (let [i, arrow] of arrows.entries()) {
+
+		arrow[1].addEventListener('click', () => {
+		
+			if(answers[i][1].classList.contains('hidden')){
+				questions[i][1].children[0].style.fontWeight = 'bold';
+				answers[i][1].classList.remove('hidden');
+				arrow[1].classList.add('arrowUp');
+			}else{
+				questions[i][1].children[0].style.fontWeight = 'normal';
+				answers[i][1].classList.add('hidden');
+				arrow[1].classList.remove('arrowUp');
+			}
+		})
+}
+
